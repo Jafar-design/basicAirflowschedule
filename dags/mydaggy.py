63162107d95ad5963ab4aeca8df18968144201d6
Jafar Akinfenwa <jafarjamiu@gmail.com>
2022-10-20 18:44:58 +0100

    Added print statements

--- dags/mydaggy.py
+++ dags/mydaggy.py
@@ -5,18 +5,21 @@ from datetime import datetime
 from airflow.decorators import task, dag
 # from include import gtb_summary
 
-
-
 now=datetime.now()
+
+
+
 @dag(schedule='* * * * *', start_date=datetime(2022,10,19), catchup=False, tags=['myFirstdag'])
 def writeToFile():
     
+    print("Code zero, just before task decorator")
     @task
     def writeInsideTxt():
-
         # This code opens a new file if it doesn't exist before, and appends current timetsamp
         with open('wr1.txt', mode='a') as file:
+            print("Starting to succeeed")
             file.write(now.strftime("%m/%d/%Y, %H:%M:%S") + '\n')
+            print("Dag should succeed now")
     writeInsideTxt = writeInsideTxt()
             
 
